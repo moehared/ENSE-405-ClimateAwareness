16b4f722b1662c2cac04b14613038fa2c7748f8c
Mohamed Bashir <mohamedbashir@Mohameds-MBP.hitronhub.home>
2021-02-25 21:00:20 -0600

    done dependency ingestion

---
+++ app/lib/screens/tabs.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Center(
+        child: Text("Home page"),
+      ),
+    );
+  }
+}
